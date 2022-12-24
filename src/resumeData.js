129a986a8ca1f837099e1a5b7d71df0d1244cca9
Manali <manalisonar@gmail.com>
2022-12-24 11:54:34 +0530

    my first commit

---
+++ src/resumeData.js
@@ -0,0 +1,113 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Manali Sonar",
+    "role": "Full Stack Developer",
+    "linkedinId":"manali-sonar-7757089",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, simply learn new things in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/manali-sonar-7757089b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Manali1892",
+          "className":"fa fa-github"
+        }
+      /*  {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }*/
+      ],
+    "aboutme":"I am currently doing job as a Sr.Associate in WNS Global Services. I have 3 years of experience in it. I learnt MERN stack developing as well as AWS.Now I am looking for a job as a Full stack developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Nasik,Maharashtra,India",
+    "mobile" :"8605196290",
+    "email"  :"manalisonar@gmail.com",
+   // "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "CollegeName":"K.T.H.M College Nashik",
+        "UniversityName":"Pune University",
+        "specialization":"MCA(Science)",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2015"
+       // "Achievements":"Some Achievements"
+      },
+      {
+        "CollegeName":"M.S.G college Malegaon",
+        "UniversityName":"Pune University",
+        "specialization":"Bsc(Computer Science)",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2012"
+       // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"WNS Global Services",
+        "Process Name":"Miles without Hours(MWH) in operations",
+        "MonthOfLeaving":"currently",
+        "YearOfLeaving":"working",
+        "Achievements":"I received League of champion award 2 times."
+      },
+      {
+        "CompanyName":"Tata Consultancy Services",
+        "Process Name":"RBL-Bajaj Credit Card Process",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2018",
+        "Achievements":"I received appreciation from clients and Promoted as QA"
+      }
+    ],
+    "skillsDescription":"React, NodeJs, Express, MongoDB, HTML, CSS, Bootstrap, SQL, AWS, MYSQL",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"MongoDB"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Euipment Rental Application",
+        "description1":"github.com/Manali1892/Hackathon2EqrentalReact",
+        "description2":"github.com/Manali1892/Hackathon2EqrentalNodejs",               
+        "imgurl":"images/portfolio/EquipRent.png"
+      },
+      {
+        "name":"Password Reset Application",
+        "description1":"github.com/Manali1892/PasswordResetReact37",
+        "description2":"github.com/Manali1892/PasswordResetNodejs37",
+        "imgurl":"images/portfolio/PwdReset.png"
+      },
+      {
+        "name":"Student Mentor Site",
+        "description1":"github.com/Manali1892/StudentMentorReact35",  
+        "description2":"github.com/Manali1892/StudentMentorNodejs35",
+        "imgurl":"images/portfolio/StudMen.png"
+      },
+      {
+        "name":"Query Resolving System",
+        "description1":"github.com/Manali1892/CapstoneQueryRes-Front",
+        "description2":"github.com/Manali1892/CapstoneQueryRes-Back",
+        "imgurl":"images/portfolio/Zen.png"
+      }
+    ]
+    
+  }
+  
+  export default resumeData
